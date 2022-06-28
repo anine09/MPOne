93cad08d5463c18a539070ba1fd5eee11c8b2920
epsilon_luoo <epsilon_luoo@outlook.com>
2022-06-29 00:05:56 +0800

    Tek Source-measure programming example.

--- demo.py
+++
@@ -1,2 +0,0 @@
-from VISA import Automatic_Instrument_Detection as autoDetect
-autoDetect.detection()
--- example/Source-measure_programming_example_tek.py
+++ example/Source-measure_programming_example_tek.py
@@ -1,58 +1,25 @@
-import pyvisa
+from VISA import Automatic_Instrument_Detection as autoDetect
+from VISA.Source_Measure.simple_source_measure import simple_source_measure as sm
+from test import Live_Data_Plot as plot
 
-# Argument List
+# 自动检测与连接设备
+tek2600B = autoDetect.detection()
+
+# 根据 Tek 手册示例写的简单的电流测量函数
+data = sm(tek2600B, "smua", "voltage", "autorange", 5, 10e-3, "current", 10e-3)
+
+# 启动图形界面，实时画图
+plot(data)
+
 """
-Source function and range: Voltage, autorange 
-Source output level: 5 V 
-Current compliance limit: 10 mA 
-Measure function and range: Current, 10 mA 
+sm电流测量函数各参数:
+tek2600B --> 这个名字代表着连接上的设备，后期我们会把它给简化掉
+smua --> 在 1 号通道进行检测，在手册里叫做通道 A，用 smua 与 smub 表示，后期应该也可以简化
+voltage --> 设置源为电压源
+autorange --> 自动进行范围设置，当然也支持手动输入最大值，但是手册上说如果规格不对会导致超出范围的错误
+5 --> 设定源电压为 5V
+第一个 10e-3 --> 设定测量电流的上限为 10mA
+current --> 要测量的对象是电流
+第二个 10e-3 --> 测量范围是 10mA，这个就是手动设置范围，会比自动设置要快
+测量的对象是可以改的，只要 voltage 和  current 调换位置，然后适当调整其他参数就行，总体来说，前5个属于源参数，后3个属于测量参数
 """
-
-# Tek Code Example
-"""
--- Restore 2600B defaults. 
-smua.reset() 
--- Select voltage source function. 
-smua.source.func = smua.OUTPUT_DCVOLTS 
--- Set source range to autorange. 
-smua.source.autorangev = smua.AUTORANGE_ON 
--- Set voltage source to 5 V. 
-smua.source.levelv = 5  
--- Set current limit to 10 mA. 
-smua.source.limiti = 10e-3 
--- Set current range to 10 mA. 
-smua.measure.rangei = 10e-3 
--- Turn on output. 
-smua.source.output = smua.OUTPUT_ON 
--- Print and place the current reading in the reading buffer. 
-print(smua.measure.i(smua.nvbuffer1)) 
--- Turn off output. 
-smua.source.output = smua.OUTPUT_OFF 
-"""
-
-
-def source_measure(device_name, channel, source_function, source_range, source_output_level, current_compliance_limit,
-                   measure_function,
-                   measure_range, debug=False):
-    if debug:
-        channel = "suma"
-        source_function = "Voltage"
-        source_range = "autorange"
-        source_output_level = "5"  # 5 V
-        current_compliance_limit = "10e-3"  # 10 mA
-        measure_function = "Current"
-        measure_range = "10e-3"  # 10 mA
-
-    device_name.write(f"{channel}.reset()")  # Restore 2600B defaults.
-    if source_function == "Voltage":
-        device_name.write(f"{channel}.source.func = {channel}.OUTPUT_DCVOLTS")  # Select voltage source function.
-    if source_range == "autorange":
-        device_name.write(f"{channel}.source.autorangev = {channel}.AUTORANGE_ON")  # Set source range to autorange.
-    device_name.write(f"{channel}.source.levelv = {source_output_level}")  # Set voltage source to 5 V.
-    device_name.write(f"{channel}.source.limiti = {current_compliance_limit}")  # Set current range to 10 mA.
-    if measure_function == "Current":
-        device_name.write(f"{channel}.measure.rangei = {measure_range}")  # Set current range to 10 mA.
-    device_name.write(f"{channel}.source.output = {channel}.OUTPUT_ON")  # Turn on output.
-    device_name.write(
-        f"{channel}.measure.i({channel}.nvbuffer1)")  # Print and place the current reading in the reading buffer.
-    device_name.write(f"{channel}.source.output = {channel}.OUTPUT_OFF ")  # Turn off output.
