5378bf31338c6fd05271b5181b89ddcaf4def9aa
Epsilon Luoo <epsilon_luoo@outlook.com>
2022-11-19 16:25:21 +0800

    Delete VISA directory

--- VISA/__init__.py
+++
@@ -1,8 +0,0 @@
-import Automatic_Instrument_Detection
-
-# version 0.0.1
-__version__ = "0.0.1"
-
-# Attention
-print("Attention! \"Live Data Plot\" function is still in Early"
-      " Development Version,\n"+"it is Unstable and supports only graphical interface.")
