72d82c55c4bed8446216b5f659f0bb7f16782247
anine09 <epsilon_luoo@outlook.com>
2023-02-13 03:57:00 +0800

    update pypi tool

--- upload_pypi.py
+++ upload_pypi.py
@@ -7,20 +7,29 @@ NOW_VERSION = "0.0.4"
 
 def fix_version():
     with open("pyproject.toml", "rt") as file:
-        x = file.read()
+        pyproject = file.read()
         
     with open("pyproject.toml", "wt") as file:
         pattern = "(?<=(version = \")).*?(?=(\"))"
-        version = time.strftime(".dev%Y%m%d%H%M", time.localtime()) 
-        x = re.sub(pattern, NOW_VERSION+version, x, count=1)
-        file.write(x)
+        if sys.platform.startswith("win32"):
+            pyproject = re.sub(pattern, NOW_VERSION, pyproject, count=1)
+            file.write(pyproject)
+        elif sys.platform.startswith("linux"):
+            version = time.strftime(".dev%Y%m%d%H%M", time.localtime()) 
+            pyproject = re.sub(pattern, NOW_VERSION+version, pyproject, count=1)
+            file.write(pyproject)
+        else:
+            raise NotImplementedError
+        
 
 
 def upload_win():
     print("del old dist")
     os.system("rmdir /q/s dist")
     print("run: python -m build")
-    os.system("python -m build")
+    # os.system("python -m build")
+    print("run: upload to pypi")
+    os.system("twine upload --repository pypi dist/*")
 
 def upload_linux():
     print("rm old dist")
