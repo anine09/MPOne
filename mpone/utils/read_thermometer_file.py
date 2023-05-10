2ea9693e38300e629bb6c2c85515b38e39854c92
anine09 <epsilon_luoo@outlook.com>
2023-05-10 13:20:46 +0800

    update read temp

--- mpone/utils/read_thermometer_file.py
+++ mpone/utils/read_thermometer_file.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def read_thermometer_file(file_path: str):
-    data = pd.read_csv(f"{file_path}", encoding="gbk", skiprows=31, names=["Date", "Time", "Temp", "drop_a","drop_b","drop_c","drop_d"]).dropna(how='any', axis=1, inplace=False)
+def read_thermometer_file(file):
+    data = pd.read_csv(file, encoding="gbk", skiprows=31, names=["Date", "Time", "Temp", "drop_a","drop_b","drop_c","drop_d"]).dropna(how='any', axis=1, inplace=False).drop("Date", axis=1)
     
     return data
